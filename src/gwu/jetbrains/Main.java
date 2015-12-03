893b8727f7560a507c644297f738263fbc510ece
aganezov <aganezov@gwu.edu>
2015-12-03 18:19:32 -0500

    added JavaDoc, don't give me an F

--- src/gwu/jetbrains/Main.java
+++ src/gwu/jetbrains/Main.java
@@ -2,6 +2,9 @@ package gwu.jetbrains;
 
 public class Main {
 
+    /*
+    Awesome JavaDoc!
+     */
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
