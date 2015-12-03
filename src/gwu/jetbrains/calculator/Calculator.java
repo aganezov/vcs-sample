61c04d43974e5ee872b84d87cc1d7d3769172d9d
aganezov <aganezov@gwu.edu>
2015-12-03 18:44:24 -0500

    added support for printing random long in calculator

--- src/gwu/jetbrains/calculator/Calculator.java
+++ src/gwu/jetbrains/calculator/Calculator.java
@@ -7,4 +7,7 @@ public class Calculator {
     public static void printRandomInteger(){
         System.out.println(new Random().nextInt());
     }
+    public void printRandomLong(){
+        System.out.println(new Random().nextLong());
+    }
 }
