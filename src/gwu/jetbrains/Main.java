199d38474485cd915810eadc95fc4b03e2fe5f3f
aganezov <aganezov@gwu.edu>
2015-12-03 18:42:44 -0500

    added a greeting from team 2

--- src/gwu/jetbrains/Main.java
+++ src/gwu/jetbrains/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     /*
     Awesome JavaDoc!
+    JavaDoc extension from team 2!
      */
     public static void main(String[] args) {
         System.out.println("Hello world!");
