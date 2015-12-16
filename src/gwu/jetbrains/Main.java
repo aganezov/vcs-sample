bbad7bf6070ff33ee6efa790952774e4a1091b20
aganezov <aganezov@gwu.edu>
2015-12-16 16:46:27 -0500

    commit 1

--- src/gwu/jetbrains/Main.java
+++ src/gwu/jetbrains/Main.java
@@ -6,6 +6,7 @@ public class Main {
     Awesome JavaDoc!
     JavaDoc extension from team 2!
      */
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world from team 2!");
