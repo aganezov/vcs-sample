f9522d3655fdf9e3103234393ae629682d2d03a0
aganezov <aganezov@gwu.edu>
2015-12-16 18:58:17 -0500

    preserver commit

--- src/gwu/jetbrains/Main.java
+++ src/gwu/jetbrains/Main.java
@@ -7,8 +7,6 @@ public class Main {
     JavaDoc extension from team 2!
      */
 
-
-
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world from team 2!");
